1c6950175f3098293cba0f89139ff9badbe3bcfd
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-02-10 22:35:33 +0200

    Create types for cards & create a deck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Suit {
+    Red,
+    Green,
+    Blue,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+enum Card {
+    Number(Suit, u32),
+    Dragon(Suit),
+    Flower,
+}
+
+fn make_deck() -> Vec<Card> {
+    let mut ret = Vec::<Card>::new();
+    ret.push(Card::Flower);
+    for suit in vec![Suit::Red, Suit::Green, Suit::Blue] {
+        for number in 1..(9 + 1) {
+            ret.push(Card::Number(suit, number));
+        }
+        ret.push(Card::Dragon(suit));
+    }
+
+    ret
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("deck: {:?}", make_deck());
 }
