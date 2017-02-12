bd59ebcb36d6e9f84ca00426a45af07d4d26a7b2
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-02-12 18:10:50 +0200

    Add some tests for is_legal_move()

--- src/main.rs
+++ src/main.rs
@@ -125,23 +125,27 @@ fn test_make_deck() {
     assert_eq!(make_deck().len(), 40);
 }
 
-#[test]
-fn test_get_card_at_nonempty() {
-    let filled = Playfield {
+fn make_test_playfield() -> Playfield {
+    Playfield {
         freecells: [FreeCell::Free, FreeCell::Flipped, FreeCell::InUse(Card::Dragon(Suit::Blue))],
         flower: Some(Card::Flower),
         piles: [None, Some(Card::Number(Suit::Green, 1)), None],
         tableau: [
-            vec![],
-            vec![Card::Dragon(Suit::Red)],
-            vec![Card::Number(Suit::Red, 3), Card::Number(Suit::Green, 2)],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
+            /* 0 */ vec![],
+            /* 1 */ vec![Card::Dragon(Suit::Red)],
+            /* 2 */ vec![Card::Number(Suit::Red, 4), Card::Number(Suit::Green, 3)],
+            /* 3 */ vec![Card::Number(Suit::Green, 2)],
+            /* 4 */ vec![Card::Number(Suit::Blue, 2)],
+            /* 5 */ vec![Card::Number(Suit::Blue, 4)],
+            /* 6 */ vec![],
+            /* 7 */ vec![],
         ]
-    };
+    }
+}
+
+#[test]
+fn test_get_card_at() {
+    let filled = make_test_playfield();
     let empty = Playfield {
         freecells: [FreeCell::Free, FreeCell::Free, FreeCell::Free],
         flower: None,
@@ -161,5 +165,33 @@ fn test_get_card_at_nonempty() {
     // Tableau
     assert_eq!(get_card_at(&filled, Position::Tableau(0)), None);
     assert_eq!(get_card_at(&filled, Position::Tableau(1)), Some(Card::Dragon(Suit::Red)));
-    assert_eq!(get_card_at(&filled, Position::Tableau(2)), Some(Card::Number(Suit::Green, 2)));
+    assert_eq!(get_card_at(&filled, Position::Tableau(2)), Some(Card::Number(Suit::Green, 3)));
+}
+
+#[test]
+fn test_is_legal_move() {
+    let playfield = make_test_playfield();
+
+    /********** Source checks *********/
+    // Moving the placed flower: not allowed
+    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Flower, Position::FreeCell(0))));
+    // Moving from the pile: not allowed
+    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Pile(1), Position::FreeCell(0))));
+    // Moving from a flipped freecell: not allowed
+    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::FreeCell(1), Position::FreeCell(0))));
+
+    /********** Destination checks *********/
+    // Move to empty freecell: true
+    assert!(is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(1), Position::FreeCell(0))));
+    // Move to flipped freecell: false
+    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(1), Position::FreeCell(1))));
+    // Move to in-use freecell: false
+    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(1), Position::FreeCell(2))));
+
+    // Moving topmost card (Green 3) to (Blue 4): Allowed
+    assert!(is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(2), Position::Tableau(5))));
+    // Moving (Green 2) on top of (Green 3): Not allowed
+    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(3), Position::Tableau(2))));
+    // Moving (Black 2) on top of (Green 3): Allowed
+    assert!(is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(4), Position::Tableau(2))));
 }
