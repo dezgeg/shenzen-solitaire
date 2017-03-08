34de521feabd1057b20979ed1256d9666f80bed0
Tuomas Tynkkynen <tuomas@tuxera.com>
2017-03-08 22:45:37 +0200

    Refactor card drawing (still pretty ugly!)

--- src/main.rs
+++ src/main.rs
@@ -22,54 +22,76 @@ fn ansi_of_dragon(suit: Suit) -> String {
     style_of_suit(suit).paint(c).to_string()
 }
 
-fn print_card(card: &Card) {
-    match *card {
-        Card::Dragon(s) => print!("│ {}      │ ", ansi_of_dragon(s)),
-        Card::Flower => print!("│  ~~~~  │ "),
-        Card::Number(s, n) => print!("│ {}      │ ", style_of_suit(s).paint(n.to_string()), ),
+fn print_card(card: &Card, is_head: bool) {
+    if is_head {
+        match *card {
+            Card::Dragon(s) => print!("│ {}      │ ", ansi_of_dragon(s)),
+            Card::Flower => print!("│  ~~~~  │ "),
+            Card::Number(s, n) => print!("│ {}      │ ", style_of_suit(s).paint(n.to_string()), ),
+        };
+    } else {
+        match *card {
+            Card::Dragon(s) => print!("│      {} │ ", ansi_of_dragon(s)),
+            Card::Flower => print!("│  ~~~~  │ "),
+            Card::Number(s, n) => print!("│      {} │ ", style_of_suit(s).paint(n.to_string()), ),
+        };
     }
 }
 
+// Card drawing: each non-topmost card consists of 1 'head' piece (where 1 piece == 2 lines)
+// and the topmost card consists of 4 pieces (head, 2 filler, tail)
+//╭────────╮\ head
+//│ 2      │/
+//╭────────╮\ head
+//│ 1      │/
+//│        |\ filler 1
+//│        │/
+//│        │\ filler 2
+//│        │/
+//│      1 │\ tail
+//╰────────╯/
+// So for a stack of n cards, we always draw n + 3 pieces.
+// Exception: empty stacks are not drawn.
+
 fn print_playfield(playfield: &Playfield) {
     let max_col_height = playfield.tableau.iter().map(|cs| cs.len()).max().unwrap();
-    for row in 0..(max_col_height + 3) {
-        for col in 0..8 {
-            let cards_in_column = &playfield.tableau[col];
-            let column_height = cards_in_column.len();
-            if row < column_height {
-                print!("╭────────╮ ");
-            } else if row >= column_height && row <= column_height + 2 && !cards_in_column.is_empty() {
-                print!("│        │ ");
-            } else {
-                print!("           ");
+    for piece_index in 0..(max_col_height + 3) {
+        for line in 1..3 {
+            for col in 0..8 {
+                let cards_in_column = &playfield.tableau[col];
+                let column_height = cards_in_column.len();
+
+                let is_head = piece_index < column_height;
+                let is_filler = !is_head && (piece_index < column_height + 2 && !cards_in_column.is_empty());
+                let is_tail = piece_index == column_height + 2 && !cards_in_column.is_empty();
+
+                if line == 1 {
+                    if is_head {
+                        print!("╭────────╮ ");
+                    } else if is_filler {
+                        print!("│        │ ");
+                    } else if is_tail {
+                        let card = cards_in_column.get(piece_index - 3).unwrap();
+                        print_card(card, false);
+                    } else {
+                        print!("           ");
+                    }
+                } else {
+                    if is_head {
+                        let card = cards_in_column.get(piece_index).unwrap();
+                        print_card(card, true);
+                    } else if is_filler {
+                        print!("│        │ ");
+                    } else if is_tail {
+                        print!("╰────────╯ ");
+                    } else {
+                        print!("           ");
+                    }
+                }
             }
+            println!();
         }
-        println!();
-        for col in 0..8 {
-            let cards_in_column = &playfield.tableau[col];
-            let column_height = cards_in_column.len();
-            if let Some(card) = cards_in_column.get(row) {
-                print_card(card);
-            } else if row < column_height + 2 && !cards_in_column.is_empty() {
-                print!("│        │ ");
-            } else if row == column_height + 2 && !cards_in_column.is_empty() {
-                print!("╰────────╯ ");
-            } else {
-                print!("           ");
-            }
-        }
-        println!();
     }
-    //for n in 0..5 {
-    //    for col in 0..8 {
-    //        print!("│        │ ");
-    //    }
-    //    println!();
-    //}
-    //for col in 0..8 {
-    //    print!("╰────────╯ ");
-    //}
-    //println!();
 }
 
 fn main() {
@@ -85,7 +107,7 @@ fn main() {
             /* 4 */ vec![Card::Number(Suit::Red, 1), Card::Number(Suit::Black, 2), Card::Number(Suit::Green, 3), Card::Number(Suit::Red, 4)],
             /* 5 */ vec![Card::Number(Suit::Red, 5), Card::Number(Suit::Black, 4)],
             /* 6 */ vec![Card::Number(Suit::Red, 6), Card::Number(Suit::Black, 5)],
-            /* 7 */ vec![Card::Number(Suit::Red, 1), Card::Number(Suit::Black, 2), Card::Number(Suit::Green, 3), Card::Number(Suit::Red, 4), Card::Number(Suit::Black, 9), Card::Number(Suit::Black, 8), Card::Number(Suit::Black, 7), Card::Number(Suit::Black, 6), Card::Number(Suit::Black, 5), Card::Number(Suit::Black, 4), Card::Number(Suit::Black, 3), Card::Number(Suit::Black, 2), Card::Number(Suit::Black, 1),],
+            /* 7 */ vec![Card::Number(Suit::Red, 1), Card::Number(Suit::Black, 2), Card::Number(Suit::Green, 3), Card::Number(Suit::Red, 4), Card::Number(Suit::Black, 9), Card::Number(Suit::Black, 8), Card::Number(Suit::Black, 7), Card::Number(Suit::Black, 6), Card::Number(Suit::Black, 5), Card::Number(Suit::Black, 4), Card::Number(Suit::Black, 3), Card::Number(Suit::Black, 2), Card::Number(Suit::Black, 1), ],
         ]
     };
     //print_playfield(&make_shuffled_playfield());
