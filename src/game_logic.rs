3a9ba3b1ed16c8e318b88d706545ad512591970e
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2017-07-07 17:54:41 +0300

    Write pickup code

--- src/game_logic.rs
+++ src/game_logic.rs
@@ -65,6 +65,21 @@ pub struct Playfield {
     pub tableau: [Vec<Card>; 8],
 }
 
+impl Clone for Playfield {
+    fn clone(&self) -> Playfield {
+        let mut tmp = [vec![], vec![], vec![], vec![], vec![], vec![], vec![], vec![]];
+        for i in 0..tmp.len() {
+            tmp[i] = self.tableau[i].clone();
+        }
+        Playfield {
+            freecells: self.freecells.clone(),
+            flower: self.flower,
+            piles: self.piles.clone(),
+            tableau: tmp,
+        }
+    }
+}
+
 pub fn make_shuffled_playfield() -> Playfield {
     let mut deck = make_shuffled_deck();
     let mut ret = Playfield {
@@ -130,39 +145,95 @@ pub enum Position {
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
-pub enum Move {
-    FlipDragon(Suit),
-    MoveCards(u32, Position, Position),
+pub struct Move(u32, Position, Position);
+
+pub fn pick_up_cards(playfield: Playfield, count: u32, from: Position) -> Option<(Playfield, Vec<Card>)> {
+    let mut pf2 : Playfield = playfield;
+    match from {
+        Position::Flower | Position::Pile(_) => return None,
+        Position::FreeCell(i) => match pf2.freecells[i as usize] {
+            FreeCell::InUse(card) => {
+                if count != 1 {
+                    return None;
+                }
+                *pf2.freecells.get_mut(i as usize).unwrap() = FreeCell::Free;
+                Some((pf2, vec![card]))
+            }
+            _ => None,
+        },
+        Position::Tableau(i) => {
+            let picked_up_cards = {
+                let old_cards = pf2.tableau.get_mut(i as usize).unwrap();
+                if count as usize > old_cards.len() {
+                    return None;
+                }
+                old_cards.split_off(count as usize - 1)
+            };
+            // XXX: check suits
+            Some((pf2, picked_up_cards))
+        }
+    }
+}
+
+pub fn place_cards(playfield: Playfield, new_cards: Vec<Card>, to: Position) -> Option<Playfield> {
+    let mut pf2 : Playfield = playfield;
+    let bottom_card = new_cards[0];
+    if let Position::Tableau(ti) = to {
+        let ok = {
+            let old_cards = &mut pf2.tableau[ti as usize];
+            let last = old_cards.last().cloned();
+            old_cards.extend(new_cards);
+            match (last, bottom_card) {
+                (None, _) => true,
+                (Some(Card::Number(dst_suit, _)), Card::Number(src_suit, _)) => src_suit != dst_suit, // XXX: check number
+                _ => false,
+            }
+        };
+        if ok {
+            return Some(pf2)
+        } else {
+            return None
+        }
+    }
+
+    if new_cards.len() != 1 {
+        return None;
+    }
+
+    let ok = match (to, bottom_card) {
+        (Position::FreeCell(fi), _) => match pf2.freecells[fi as usize]{
+            FreeCell::Free => {
+                pf2.freecells[fi as usize] = FreeCell::InUse(bottom_card);
+                true
+            }
+            _ => false,
+        },
+        (Position::Flower, Card::Flower) => {
+            pf2.flower = Some(bottom_card);
+            true
+        },
+        (Position::Pile(pi), Card::Number(src_suit, src_number)) => {
+            let pile = &mut pf2.piles[pi as usize];
+            let last = *pile;
+            *pile = Some(bottom_card);
+            match last {
+                Some(Card::Number(dst_suit, dst_number)) => src_suit == dst_suit && src_number == dst_number + 1,
+                _ => true,
+            }
+        },
+        _ => false,
+    };
+    if ok { Some(pf2) } else { None }
 }
 
 pub fn is_legal_move(playfield: &Playfield, m: Move) -> bool {
-    match m {
-        Move::FlipDragon(_) => unreachable!(),
-        Move::MoveCards(count, from, to) => {
-            // Evaluate source card, or bail out if no source card or not allowed to move from source
-            let src_card = match from {
-                Position::Flower | Position::Pile(_) => return false,
-                x => if let Some(card) = get_card_at(playfield, x) { card } else { return false; },
-            };
-            match to {
-                Position::FreeCell(i) => playfield.freecells[i as usize] == FreeCell::Free,
-                Position::Flower => src_card == Card::Flower,
-                Position::Tableau(_) => {
-                    match (src_card, get_card_at(playfield, to)) {
-                        (Card::Number(src_suit, src_number), Some(Card::Number(dst_suit, dst_number))) =>
-                            return src_number == dst_number - 1 && src_suit != dst_suit,
-                        (_, None) => return true,
-                        _ => return false,
-                    }
-                }
-                Position::Pile(_) => {
-                    match (src_card, get_card_at(playfield, to)) {
-                        (Card::Number(src_suit, src_number), Some(Card::Number(dst_suit, dst_number))) =>
-                            return src_suit == dst_suit && src_number == dst_number + 1,
-                        (Card::Number(_, 1), None) => return true,
-                        _ => return false,
-                    }
-                }
+    let Move(count, from, to) = m;
+    match pick_up_cards((*playfield).clone(), count, from) {
+        None => false,
+        Some((new_pf, picked_up_cards)) => {
+            match place_cards(new_pf, picked_up_cards, to) {
+                Some(_) => true,
+                None => false,
             }
         }
     }
@@ -192,24 +263,24 @@ fn test_is_legal_move() {
 
     /********** Source checks *********/
     // Moving the placed flower: not allowed
-    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Flower, Position::FreeCell(0))));
+    assert!(!is_legal_move(&playfield, Move(1, Position::Flower, Position::FreeCell(0))));
     // Moving from the pile: not allowed
-    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Pile(1), Position::FreeCell(0))));
+    assert!(!is_legal_move(&playfield, Move(1, Position::Pile(1), Position::FreeCell(0))));
     // Moving from a flipped freecell: not allowed
-    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::FreeCell(1), Position::FreeCell(0))));
+    assert!(!is_legal_move(&playfield, Move(1, Position::FreeCell(1), Position::FreeCell(0))));
 
     /********** Destination checks *********/
     // Move to empty freecell: true
-    assert!(is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(1), Position::FreeCell(0))));
+    assert!(is_legal_move(&playfield, Move(1, Position::Tableau(1), Position::FreeCell(0))));
     // Move to flipped freecell: false
-    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(1), Position::FreeCell(1))));
+    assert!(!is_legal_move(&playfield, Move(1, Position::Tableau(1), Position::FreeCell(1))));
     // Move to in-use freecell: false
-    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(1), Position::FreeCell(2))));
+    assert!(!is_legal_move(&playfield, Move(1, Position::Tableau(1), Position::FreeCell(2))));
 
     // Moving topmost card (Green 3) to (Black 4): Allowed
-    assert!(is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(2), Position::Tableau(5))));
+    assert!(is_legal_move(&playfield, Move(1, Position::Tableau(2), Position::Tableau(5))));
     // Moving (Green 2) on top of (Green 3): Not allowed
-    assert!(!is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(3), Position::Tableau(2))));
+    assert!(!is_legal_move(&playfield, Move(1, Position::Tableau(3), Position::Tableau(2))));
     // Moving (Black 2) on top of (Green 3): Allowed
-    assert!(is_legal_move(&playfield, Move::MoveCards(1, Position::Tableau(4), Position::Tableau(2))));
+    assert!(is_legal_move(&playfield, Move(1, Position::Tableau(4), Position::Tableau(2))));
 }
